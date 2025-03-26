0b1c0b5cd155d8e82f93004ed6fb2a7085b4ab6f
awakra <lucasadls0@gmail.com>
2025-03-25 23:52:52 -0300

    Nível Novato - Concluído.

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main()
+{
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,6 +28,27 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    int i;
+    int const torre = 5, bispo = 5, rainha = 8; // Quantidade de casas por peça.
+    printf("********** Movimentacao da Torre **********\n");
+    for (i = 0; i < torre; i++)
+    {
+        printf("Direita\n");
+    }
+    i = 0; // Reseta variável de controle.
+    printf("********** Movimentacao do Bispo **********\n");
+    while (i < bispo)
+    {
+        printf("Cima, Direita\n");
+        i++;
+    }
+    i = 0; // Reseta variável de controle.
+    printf("********** Movimentacao da Rainha **********\n");
+    do
+    {
+        printf("Esquerda\n");
+        i++;
+    } while (i < rainha);
 
     return 0;
 }
